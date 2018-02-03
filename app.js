d3e87c0333075e1cccd02a654e878eb07f9ca370
Jacob Albright <jacob.albright23@gmail.com>
2018-02-03 15:27:20 -0600

    Application now includes some form validation. It's styled a lot more professionally.

--- app.js
+++ app.js
@@ -2,11 +2,6 @@ var employees = []; // current employees on the DOM
 var removedEmployees = []; // employees that have been removed from the DOM
 var employeesPastAndPresent = []; // all employees ever to have exisited
 
-employees.push(new Employee('Jacob', 'Albright', 'Phone', 13322, 50000));
-employees.push(new Employee('Caroline', 'Cruys', 'Social Worker', 44932, 70000));
-employees.push(new Employee('Jacob', 'Allers', 'Artist', 44998, 55000));
-
-
 function Employee(first, last, title, id, salary){ // constructor for Employee object
   this.firstName = first;
   this.lastName = last;
@@ -17,35 +12,46 @@ function Employee(first, last, title, id, salary){ // constructor for Employee o
 
 $(document).ready(function(){
   displayInfoOnTable(employees);
-  $('.inputDiv').on('click', function(e){
-    if(e.target && !e.target.matches('.inputel')){
-      $('#firstNameIn').focus();
-    }
-  }); // end focus div
-
+  $('.inputDiv').on('click', focus);
   $('#submitBtn').on('click', function(){
     getFormInfo(); // create an Employee instane and push it to the appropriate arrays
     displayInfoOnTable(employees); // pass the emploees array to displayInfoOnTable...could display another array if desired
-    console.log(employees);
   }); // end #submitBtn click
-
-  $('#infoTable').on('click', '.removeBtn', function(){
-    for (var i = 0; i < employees.length; i++) {
-      if($(this).data('id') === employees[i].employeeId){
-        console.log($(this).data('id') + ' and ' + employees[i].employeeId + ' = a match!');
-        removedEmployees.push(employees.splice(i, 1));
-        displayInfoOnTable(employees);
-      }
-    }
-  });
-
+  $('#infoTable').on('click', '.removeBtn', removeEmployee);
 }); // end document.ready
 
+function focus(e){
+  if(e.target && !e.target.matches('.inputel')){
+    $('#firstNameIn').focus();
+  }
+}
+
+function removeEmployee(){
+  for (var i = 0; i < employees.length; i++) {
+    if($(this).data('id') === employees[i].employeeId){
+      removedEmployees.push(employees.splice(i, 1));
+      displayInfoOnTable(employees);
+    }
+  }
+}
+
 function getFormInfo(){ // is called when #submitBtn is clicked
+
   $employee = new Employee($('#firstNameIn').val(), $('#lastNameIn').val(), $('#jobTitle').val(), Number($('#employeeId').val()), Number($('#salary').val())); // store a new instance of an Employee object inside $employee. Use the .inputel's .val()'s as the new Employee's property values
-  employees.push($employee); // push the new $emploee to the emploees array
-  employeesPastAndPresent.push($employee); // also push it to the employeesPastAndPresent array which will hold a record of deleted and current employees
-  $('.inputel').val(''); // clear any text inside the .inputel's
+  var valid = true; // use like a lightswitch
+  var keys = Object.keys($employee); // store the keys of each of $employee's properties
+  for(i = 0; i < keys.length; i++){
+    if(!$employee[keys[i]]){ // if any of $employee's keys is empty...
+      alert('please complete all fields'); // yell at the user
+      valid = false; // valid = false
+      break; // break so the user only gets one message
+    }
+  }
+  if(valid === true){ // if all of the employee's keys hold values...
+    employees.push($employee); // push the new $emploee to the emploees array
+    employeesPastAndPresent.push($employee); // also push it to the employeesPastAndPresent array which will hold a record of deleted and current employees
+    $('.inputel').val(''); // clear any text inside the .inputel's
+  }
 } // end getFormInfo
 
 function displayInfoOnTable(arr){
@@ -61,16 +67,7 @@ function displayInfoOnTable(arr){
     $tr.append($('<button data-id="'+employees[row].employeeId+'">').addClass('removeBtn').text('x')); // append button with data attribute
     $('.outputDiv tbody').append($tr); // append the newly filled out <tr> to the table body that's inside the .outputDiv
     totalCost += employees[row].annualSalary; // add the current employee's anual salary to total cost
-    $('#totalCost').html('Total Spent on Salaries/Month: <strong>$'+Math.round((totalCost / 12) * 100) / 100+'</strong>'); // display monthly cost on table
+    $('#totalCost').html('<p>Total Spent on Salaries/Month: $'+Math.round((totalCost / 12) * 100) / 100+'</p>'); // display monthly cost on table
   } // end row for loop
 
 } // end displayInfoOnTable
-
-
-
-
-/*
-
-
-
-*/
