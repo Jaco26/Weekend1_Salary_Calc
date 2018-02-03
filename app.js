09696bc3ce90b0ed1e7026b69655fca6865ea756
Jacob Albright <jacob.albright23@gmail.com>
2018-02-03 12:24:06 -0600

    removeBtn now working and has useful data attribute

--- app.js
+++ app.js
@@ -17,15 +17,27 @@ $(document).ready(function(){
       $('#firstNameIn').focus();
     }
   }); // end focus div
-  // push input info to employees as Employee object instance
+
   $('#submitBtn').on('click', function(){
     getFormInfo(); // create an Employee instane and push it to the appropriate arrays
     displayInfoOnTable(employees); // pass the emploees array to displayInfoOnTable...could display another array if desired
+    console.log(employees);
+  }); // end #submitBtn click
+
+  $('tbody').on('click', '.removeBtn', function(){
+    for (var i = 0; i < employees.length; i++) {
+      if($(this).data('id') === employees[i].employeeId){
+        console.log($(this).data('id') + ' and ' + employees[i].employeeId + ' = a match!');
+        removedEmployees.push(employees.splice(i));
+        displayInfoOnTable(employees);
+      }
+    }
   });
+
 }); // end document.ready
 
 function getFormInfo(){ // is called when #submitBtn is clicked
-  $employee = new Employee($('#firstNameIn').val(), $('#lastNameIn').val(), $('#jobTitle').val(), $('#employeeId').val(), $('#salary').val()); // store a new instance of an Employee object inside $employee. Use the .inputel's .val()'s as the new Employee's property values
+  $employee = new Employee($('#firstNameIn').val(), $('#lastNameIn').val(), $('#jobTitle').val(), Number($('#employeeId').val()), $('#salary').val()); // store a new instance of an Employee object inside $employee. Use the .inputel's .val()'s as the new Employee's property values
   employees.push($employee); // push the new $emploee to the emploees array
   employeesPastAndPresent.push($employee); // also push it to the employeesPastAndPresent array which will hold a record of deleted and current employees
   $('.inputel').val(''); // clear any text inside the .inputel's
@@ -36,10 +48,20 @@ function displayInfoOnTable(arr){
   for(row = 0; row < arr.length; row++){ // iterate a loop once for each table row desired
     $tr = $('<tr>'); // create a new table row
     var keys = Object.keys(arr[row]); // get the keys for the properties of the object in arr at the current index
-    for (var col = 0; col < keys.length; col++) { // iterate a loop once for each table column desired
+    for (var col = 0; col < keys.length; col++) { // iterate a loop once for each table column desired...the + 1 is to make room for a button that will hold .data() and remove the row from the DOM
       $tr.append($('<td>').text(employees[row][keys[col]])[0]); // for each property in arr[row], append a new table cell with text corrosponding to the current property key to newly created table row.
     } // end column for loop
-    $('.outputDiv tbody').append($tr.data('id', this.employeeId)); // append the newly filled out <tr> to the table body that's inside the .outputDiv
+    $tr.append($('<button data-id="'+employees[row].employeeId+'">').addClass('removeBtn').text('x')); // append button with data attribute
+    $('.outputDiv tbody').append($tr); // append the newly filled out <tr> to the table body that's inside the .outputDiv
   } // end row for loop
 
 } // end displayInfoOnTable
+
+
+
+
+/*
+
+
+
+*/
