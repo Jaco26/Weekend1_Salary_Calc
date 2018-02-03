a7bb524e4de8b58abf8bd7104252cd0a2a018253
Jacob Albright <jacob.albright23@gmail.com>
2018-02-03 00:03:40 -0600

    Set up for next steps. Not really any significant changes

--- app.js
+++ app.js
@@ -11,11 +11,15 @@ function Employee(first, last, title, id, salary){ // constructor for Employee o
 }
 
 $(document).ready(function(){
-  $('#submitBtn').on('click', getFormInfo); // push input info to employees as Employee object instance
+
   $('.inputDiv').on('click', function(e){
     if(e.target && !e.target.matches('.inputel')){
       $('#firstNameIn').focus();
     }
+  }); // end focus div
+  // push input info to employees as Employee object instance
+  $('#submitBtn').on('click', function(){
+    getFormInfo();
   });
 });
 
@@ -23,4 +27,8 @@ function getFormInfo(){ // is called when #submitBtn is clicked
   $employee = new Employee($('#firstNameIn').val(), $('#lastNameIn').val(), $('#jobTitle').val(), $('#employeeId').val(), $('#salary').val());
   employees.push($employee);
   $('.inputel').val('');
+}
+
+function displayFormInfo(){
+  
 }
